29847f76e6b6a8bac70d12b3af49abc605a7efe5
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2021-12-22 12:49:49 -0400

    LocalStorage set for username

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,38 @@
-const chatroom = new Chatroom('general', 'shaun');
+// dom queries
+const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
 
-chatroom.getChats((data) => {
-    console.log(data);
+
+// add a new chat
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+    .then(() => newChatForm.reset())
+    .catch(error => console.log(error));
 });
+
+// update username
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault();
+    // update name via chatroom
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+    // reset the form
+    newNameForm.reset();
+    // show then hide the update message 
+    updateMssg.innerText = `Your name was updated to ${newName}`;
+    setTimeout(() => updateMssg.innerText = '', 3000);
+});
+
+// check local storage for a name
+const username = localStorage.username ? localStorage.username : 'anon';
+
+// class instances 
+const chatUI = new ChatUI(chatList);
+const chatroom = new Chatroom('general', username);
+
+// get chats and render
+chatroom.getChats(data => chatUI.render(data));
